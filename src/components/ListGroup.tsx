219faf086ee38e4c4ea6d7b04a4ac3d1b728d171
Muhammad Afriyano <codinglogos@gmail.com>
2023-04-05 15:09:02 +0700

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,28 @@
+function ListGroup() {
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    items = [];
+
+    const getMessage = () => {
+        return (items.length === 0 ? <p>No Item Found</p> : null)
+    }
+
+    return (
+        <>
+            <h1>List</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map(item => <li key={item}>{item}</li>)}
+
+            </ul>
+        </>
+    );
+};
+
+export default ListGroup;
